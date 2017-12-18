c9ca07472daa58ce12f16823e6aa46a6dc0ef7ca
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-12-18 23:12:32 +0100

    first commit

--- gogo.go
+++ gogo.go
@@ -1,9 +1,44 @@
+package main
 
+import (
+	"fmt"
+	"math/rand"
+	"math"
+)
 
-package  main
-import "fmt"
+func foo() {
+	fmt.Println("The square root of 4 is:", math.Sqrt(4))
+}
 
-func main()  {
-	fmt.Println("Welcome to go!")
-	
+func add(x, y float64) float64 {
+	return x + y
+
+}
+
+func multiple(a, b string) (string, string) {
+	return a, b
+
+}
+func main() {
+	foo()
+	fmt.Println("A number from 1-100: ", rand.Intn(100))
+	var num1, num2 = 5.0, 4.6
+	//var num2 float64 = 12.55
+	fmt.Println("Adding:", add(num1, num2))
+
+	var w1, w2 = "Hey there", "brown cow"
+
+	fmt.Print("Multiplication: ")
+	fmt.Println(multiple(w1, w2))
+
+	var a int = 43
+	var b float64 = float64(a)
+
+	var x = a
+	y := b
+
+	fmt.Println(x)
+	fmt.Println(" ")
+	fmt.Println(y)
+
 }
