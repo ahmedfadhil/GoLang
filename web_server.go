fc9d8977bbbd3dd962f099e60e8e0ebe7c95289d
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-12-23 12:23:15 +0100

    add pointer receivers and create objects in function

--- web_server.go
+++ web_server.go
@@ -26,23 +26,41 @@ type car struct {
 	top_speed_kmh  float64
 }
 
-func (c car) kmh() float64  {
-	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax)
-	
+// Value receivers
+func (c car) kmh() float64 {
+	c.top_speed_kmh = 500
+	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax)
+
 }
 
-func (c car) mph() float64  {
-	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax/kmh_multiple)
+func (c car) mph() float64 {
+	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax / kmh_multiple)
 
+}
+
+// Pointer receivers
+func (c *car) new_top_speed(newspeed float64) {
+	c.top_speed_kmh = newspeed
+
+}
+
+func newer_top_speed(c car, speed float64) car {
+	c.top_speed_kmh = speed
+	return c
 }
 
 func main() {
 	a_car := car{gas_pedal: 65000,
-	brake_pedal: 0,
-	steering_wheel: 1256,
-	top_speed_kmh: 225.0}
+		brake_pedal: 0,
+		steering_wheel: 1256,
+		top_speed_kmh: 225.0}
 	fmt.Println(a_car.gas_pedal)
 	fmt.Println(a_car.kmh())
 	fmt.Println(a_car.mph())
+	//a_car.new_top_speed(500)
+	a_car = newer_top_speed(a_car,500)
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
+
 
 }
