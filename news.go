ec31a007926698e0b18dea59c93a414ae96e7393
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-12-25 01:05:46 +0100

    accessed xml data on website

--- news.go
+++ news.go
@@ -20,26 +20,38 @@ var washPostXML = []byte(`
 `)
 
 type SitemapIndex struct {
-	Locations [] Location `xml:"sitemap"`
+	Locations [] string `xml:"sitemap>loc"`
 }
 
-type Location struct {
-	Loc string `xml:"loc"`
+type News struct {
+	Titles   [] string `xml:"url>news>title"`
+	Keywords [] string `xml:"url>news>keywords"`
+	Location [] string `xml:"url>loc"`
 }
 
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
-
-}
+//type Location struct {
+//	Loc string `xml:"loc"`
+//}
+//
+//func (l Location) String() string {
+//	return fmt.Sprintf(l.Loc)
+//
+//}
 
 func main() {
 	//resp, _ := http.Get("https://www.washingtonpost.com/news/sports/wp/2017/12/23/pressures-off-joe-flacco-and-john-harbaugh-with-ravens-nearing-playoff-berth/?hpid=hp_no-name_hp-in-the-news%3Apage%2Fin-the-news&utm_term=.5ebb5cc8928d")
-	bytes := washPostXML
 	var s SitemapIndex
+	var n News
+
+	bytes := washPostXML
 	xml.Unmarshal(bytes, &s)
 	fmt.Println(s.Locations)
 
 	for _, Location := range s.Locations {
 		fmt.Printf("\n%s", Location)
+
+		bytes := washPostXML
+		xml.Unmarshal(bytes, &s)
+		//fmt.Println(s.Locations)
 	}
 }
